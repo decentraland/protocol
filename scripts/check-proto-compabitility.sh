1b9bbd5312a178353d982f77a8748675a783b4df
Leandro Mendoza <leandro@decentraland.org>
2022-06-23 12:17:05 -0300

    wip

--- scripts/check-proto-compabitility.sh
+++ scripts/check-proto-compabitility.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
 
 # Download the main branch ref zip
+protocol_main_zip_url="https://github.com/decentraland/protocol/archive/refs/heads/main.zip"
+protocol_main_zip_local="./protocol-main.zip"
+
+curl -L "$protocol_main_zip_url" -o "$protocol_main_zip_local"
+rm -rf ./temp || true
+unzip "$protocol_main_zip_local" -d ./temp
+rm "$protocol_main_zip_local" || true
 
 
-echo "TODO from the bash script"
+# Run the `proto-compatibility-tool` and exclude the downloaded folder.
+echo "Checking the compatibility against $base_url"
+for i in `find * -name "*.proto" -type f`; do
+    if [[ $i != temp* ]]; then
+        proto-compatibility-tool "$i" "./temp/protocol-main/${i}"
+    fi
+done
+
+rm -rf ./temp || true
