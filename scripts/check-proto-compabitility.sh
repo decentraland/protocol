6ce832aef359e220337697ef0e7a5aac6706fe78
Leandro Mendoza <leandro@decentraland.org>
2022-06-27 10:56:25 -0300

    feat: add publish ci

--- scripts/check-proto-compabitility.sh
+++ scripts/check-proto-compabitility.sh
@@ -1,4 +1,22 @@
+#!/bin/bash
+set -e -x
 
 # Download the main branch ref zip
+protocol_main_zip_url="https://github.com/decentraland/protocol/archive/refs/heads/main.zip"
+protocol_main_zip_local="./protocol-main.zip"
+
+curl -L "$protocol_main_zip_url" -o "$protocol_main_zip_local"
+rm -rf ./temp || true
+unzip "$protocol_main_zip_local" -d ./temp
+rm "$protocol_main_zip_local" || true
 
 
+# Run the `proto-compatibility-tool` and exclude the downloaded folder.
+echo "Checking the compatibility against $base_url"
+for i in `find * -name "*.proto" -type f`; do
+    if [[ $i != temp* ]]; then
+        proto-compatibility-tool "$i" "./temp/protocol-main/${i}"
+    fi
+done
+
+rm -rf ./temp || true
