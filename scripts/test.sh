5e57a7a7626d34ccee7d3f82d7f0b550fc0ee3bf
Leandro Mendoza <leandro@decentraland.org>
2022-10-11 18:32:38 -0300

    wip

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,19 +8,19 @@ rm -rf "$(pwd)/out-cs" || true
 mkdir -p "$(pwd)/out-ts"
 mkdir -p "$(pwd)/out-cs"
 
-for file in "$(pwd)"/*.proto; do 
+for file in "$(pwd)"/tests/*.proto; do 
   echo "> Generating $file"
   node_modules/.bin/protoc \
     --plugin=./node_modules/.bin/protoc-gen-ts_proto \
     --csharp_out="$(pwd)/out-cs" \
     --ts_proto_opt=esModuleInterop=true,returnObservable=false,outputServices=generic-definitions,fileSuffix=.gen \
     --ts_proto_out="$(pwd)/out-ts" \
-    -I="$(pwd)/node_modules/protobufjs" \
-    -I="$(pwd)" \
+    -I="$(pwd)/proto" \
+    -I="$(pwd)/tests" \
     "$file";
 done
 
-echo "> Compiling TS project $file"
+echo "> Compiling TS project"
 
 node_modules/.bin/tsc -p tsconfig.json
 
