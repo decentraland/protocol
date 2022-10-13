bbb70eff962df6ff8f7bad99be73ae6d5ea02384
Leandro Mendoza <leandro@decentraland.org>
2022-10-12 23:07:21 -0300

    chore: remove google dep commited

--- scripts/install-buf.sh
+++
@@ -1,5 +0,0 @@
-# Substitute BIN for your bin directory.# Substitute VERSION for the current released version.
-BIN="./node_modules/.bin" 
-VERSION="1.8.0"
-curl -sSL "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m)" -o "${BIN}/buf"
-chmod +x "${BIN}/buf"
