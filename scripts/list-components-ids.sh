935f0416330744e0e0dd7a5db5b7704682c242d0
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2024-12-05 12:00:40 +0100

    Added sorting to id list tool

--- scripts/list-components-ids.sh
+++ scripts/list-components-ids.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ids=$(find . -name "*.proto" -exec grep -ho 'option (common\.ecs_component_id) = [0-9]\+;' {} + | sed -E 's/.*= ([0-9]+);/\1/')
+ids=$(find . -name "*.proto" -exec grep -ho 'option (common\.ecs_component_id) = [0-9]\+;' {} + | sed -E 's/.*= ([0-9]+);/\1/' | sort -n)
 
 count=$(echo "$ids" | wc -l | xargs)
 
